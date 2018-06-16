c91d68599c38c9767c8a86b6a259f5ebf4905b87
Rudy Matela <rudy@matela.com.br>
2018-06-16 15:37:08 -0300

    add missing copyright notices

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,3 +1,12 @@
+-- |
+-- Module      : Test
+-- Copyright   : (c) 2018 Rudy Matela
+-- License     : 3-Clause BSD  (see the file LICENSE)
+-- Maintainer  : Rudy Matela <rudy@matela.com.br>
+--
+-- This module is part of PercentFormat.
+--
+-- Some helper functios to test PercentFormat itself.
 module Test where
 
 import System.Environment (getArgs)
