5d28e921b917096d6425c6b9cc6810b8ff03fcd9
Rudy Matela <rudy@matela.com.br>
2018-06-16 17:04:05 -0300

    percent-s: take format string as argument

--- bench/percent-s.hs
+++ bench/percent-s.hs
@@ -2,17 +2,23 @@
 -- Distributed under the 3-Clause BSD licence (see the file LICENSE).
 import Text.PercentFormat
 import Test.LeanCheck
+import System.Environment (getArgs)
 
-
--- TODO: take the format string as a parameter
-maini :: String -> String
-maini _ = unlines $ ("%s" %&) `map` take 12 (list :: [String])
-                 ++ ("%s" %&) `map` take 12 (list :: [Int])
-                 ++ ("%s" %&) `map` take 12 (list :: [Float])
-                 ++ ("%s" %&) `map` take 12 (list :: [Rational])
+put :: String -> IO ()
+put s  =  putStrLn . unlines
+       $  (s %&) `map` take 12 (list :: [String])
+       ++ (s %&) `map` take 12 (list :: [Int])
+       ++ (s %&) `map` take 12 (list :: [Float])
+       ++ (s %&) `map` take 12 (list :: [Rational])
+       ++ (s %&) `map` take 12 (list :: [Char])
 
 main :: IO ()
-main = interact maini
+main = do
+  as <- getArgs
+  let s = case as of
+          [] -> "%r"
+          (s:_) -> s
+  put s
 
 (%&) :: Show a => String -> a -> String
 s %& x = "%r -%% %12-s == %r" % s % show11 x -% (s -% x)
