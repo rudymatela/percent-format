14d13ce481fd2f8d03cf070f38f9f777bd8c8993
Rudy Matela <rudy@matela.com.br>
2018-07-31 13:24:39 -0300

    fix haddock

--- src/Text/PercentFormat.hs
+++ src/Text/PercentFormat.hs
@@ -218,7 +218,7 @@
 -- > > "%d %i %f %.2f" Zero One Two Three
 -- > "0 1 2 3.00"
 --
--- Because when 'show'ed, values of this @Digit@ type are represented as 'Integers'.
+-- Because when 'show'ed, values of this @Digit@ type are represented as 'Integer's.
 --
 --
 -- == Error Handling
